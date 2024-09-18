74afc0fefc046f155b75c4e54d178f18926a5b02
rishijain2002 <rishijain0652@gmail.com>
2024-09-18 12:58:32 +0530

    login page

---
+++ app/login/page.tsx
@@ -0,0 +1,13 @@
+import Login from '@/components/Login';
+import React from 'react';
+
+// Ensure the component is capitalized to follow React naming conventions
+const LoginPage = () => {
+  return (
+    <div>
+      <Login />
+    </div>
+  );
+};
+
+export default LoginPage;
