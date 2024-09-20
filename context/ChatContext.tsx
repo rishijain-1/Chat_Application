725f14fd6ef7863110a1361dad9b517460ce4f82
rishijain2002 <rishijain0652@gmail.com>
2024-09-20 19:35:37 +0530

    Remove User

--- context/ChatContext.tsx
+++ context/ChatContext.tsx
@@ -1,16 +1,16 @@
-// context/ChatContext.tsx
 'use client'
 import React, { createContext, useContext, useState } from 'react';
 
-interface ChatUser {
+export interface ChatUser {
   id: string;
   name: string;
   email: string;
 }
 
-interface ChatContextProps {
+export interface ChatContextProps {
   user: ChatUser | null;
   setUser: (user: ChatUser) => void;
+  removeUser: () => void; 
 }
 
 const ChatContext = createContext<ChatContextProps | undefined>(undefined);
@@ -18,8 +18,12 @@ const ChatContext = createContext<ChatContextProps | undefined>(undefined);
 export const ChatProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [user, setUser] = useState<ChatUser | null>(null);
 
+  const removeUser = () => {
+    setUser(null); 
+  };
+
   return (
-    <ChatContext.Provider value={{ user, setUser }}>
+    <ChatContext.Provider value={{ user, setUser, removeUser }}>
       {children}
     </ChatContext.Provider>
   );
