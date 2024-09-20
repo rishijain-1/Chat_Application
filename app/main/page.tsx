0b3f97fb0c65f49423e2a9fb817461e8c770a842
rishijain2002 <rishijain0652@gmail.com>
2024-09-20 19:28:17 +0530

    Add Navbar

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,19 +2,23 @@
 
 import Chat from '@/components/chatBox';
 import ChatList from '@/components/ChatList';
+import Navbar from '@/components/Navbar';
 import React from 'react';
 
 const Main = () => {
   return (
-    <div className="flex" style={{ height: '90vh' }}>
+    <>
+      <Navbar/>
+      <div className="flex" style={{ height: '90vh' }}>
 
-      <div className="w-1/3 h-full bg-gray-200 p-4">
-        <ChatList />
+        <div className="w-1/3 h-full bg-gray-200 p-4">
+          <ChatList />
+        </div>
+        <div className="w-2/3 ">
+          <Chat />
+        </div>
       </div>
-      <div className="w-2/3 ">
-        <Chat />
-      </div>
-    </div>
+    </>
   );
 };
 
