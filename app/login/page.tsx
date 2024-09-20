29212be09256d832a0dda10f145a5c6a62f79871
rishijain2002 <rishijain0652@gmail.com>
2024-09-20 19:28:51 +0530

    Add LogoHeader

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,10 +1,12 @@
 import Login from '@/components/Login';
+import LogoHeader from '@/components/LogoHeader';
 import React from 'react';
 
 // Ensure the component is capitalized to follow React naming conventions
 const LoginPage = () => {
   return (
     <div>
+      <LogoHeader/>
       <Login />
     </div>
   );
